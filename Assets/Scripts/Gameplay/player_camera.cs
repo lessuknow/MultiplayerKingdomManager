5f16c9fe75de595533d022c66f57882232ca5a5a
Millmoss <omelliff@yahoo.com>
2020-11-08 00:53:36 -0500

    initial commit of branch millmoss

---
+++ Assets/Scripts/Gameplay/player_camera.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_camera : MonoBehaviour
+{
+	Quaternion rotation = Quaternion.identity;
+    
+    void Start()
+    {
+        
+    }
+	
+    void Update()
+    {
+        
+    }
+}
