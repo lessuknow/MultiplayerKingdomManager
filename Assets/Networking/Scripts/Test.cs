60ade0a26921ab94156e84e2b160987f785e65cb
lessuknow <cchenonetwothree@gmail.com>
2020-11-14 18:07:06 -0500

    [10] Added 'lessuknow' schene for personal testing along with installing Mirror and updating the gitignore to not pass around Mirror's files

---
+++ Assets/Networking/Scripts/Test.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using Mirror;
+
+public class Test : NetworkBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("E");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            this.transform.position += new Vector3(0, 0.25f, 0);
+        }
+    }
+}
