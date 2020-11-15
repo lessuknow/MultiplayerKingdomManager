69938b01595573300e244647910fca527a10c81a
Millmoss <omelliff@yahoo.com>
2020-11-14 19:50:02 -0500

    debug_util, camera

--- Assets/Gameplay/Scripts/Camera/player_camera.cs
+++ Assets/Gameplay/Scripts/Camera/player_camera.cs
@@ -9,23 +9,33 @@ public class player_camera : MonoBehaviour
 
 	public const float sensitivity_x = 5.0f;
 	public const float sensitivity_y = 5.0f;
-
+	
 	void Start()
 	{
-		if (camera == null)
-		{
-			Debug.LogWarning("camera is null!");
-		}
+		debug.print_warning_if(camera == null, "camera is null!");
+		debug.print_warning_if(target == null, "target is null!");
 
 		camera.transform.position = target.position;
+		debug.print_line("player_camera of user " + "-1" + " initialized");	// TODO : figure out a way to identify who's camera this is in the future
     }
 
 	void Update()
 	{
 		float mouse_x = Input.GetAxis("Mouse X");
 		float mouse_y = Input.GetAxis("Mouse Y");
-
+		
 		camera.transform.Rotate(Vector3.up, mouse_x * sensitivity_x, Space.World);
 		camera.transform.Rotate(Vector3.right, -mouse_y * sensitivity_y, Space.Self);
+		
+		if (camera.transform.up.y < 0)
+		{
+			// camera upside-down, clamp the rotation
+			//Vector3 rotation = camera.transform.localRotation.eulerAngles;
+			//float clamped_rotation_x = mouse_y > 0 ? 270 : 90;
+			//debug.print_line(clamped_rotation_x + "");
+			//rotation = new Vector3(clamped_rotation_x, rotation.y, rotation.z);
+			//camera.transform.localRotation = Quaternion.Euler(rotation);
+			//debug.print_line(camera.transform.localRotation.eulerAngles + "wow");
+		}
 	}
 }
---
+++ Assets/util/scripts/debug_util.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+
+public static class debug
+{
+	// A class to hold various debug util functions.
+
+	// XXX : some of these are redundant, but we may want different or extra
+	//		handling for warnings errors in the future. Plus, same format for
+	//		all prints is nice.
+
+	/* Example Functions */
+
+	/// <summary>
+	/// This example function takes your text, ignores it, and prints "Hello World!".
+	/// </summary>
+	/// <param name="text">Text that this function will completely ignore.</param>
+	public static void example_func(string text)
+	{
+		debug.print_line("Hello World!");
+	}
+
+	/* Log Functions */
+
+	/// <summary>
+	/// Log console message with `text`.
+	/// </summary>
+	public static void print_line(string text)
+	{
+		Debug.Log(text);
+	}
+
+	/// <summary>
+	/// Log console message with `text` if `value` is true.
+	/// </summary>
+	public static void print_line_if(bool value, string text)
+	{
+		if (value)
+		{
+			Debug.Log(text);
+		}
+	}
+
+	/// <summary>
+	/// Log warning with `text`.
+	/// </summary>
+	public static void print_warning(string text)
+	{
+		Debug.LogWarning(text);
+	}
+
+	/// <summary>
+	/// Log warning with `text` if `value` is true.
+	/// </summary>
+	public static void print_warning_if(bool value, string text)
+	{
+		if (value)
+		{
+			Debug.LogWarning(text);
+		}
+	}
+
+	/// <summary>
+	/// Log error with `text`.
+	/// </summary>
+	public static void print_error(string text)
+	{
+		Debug.LogError(text);
+	}
+
+	/// <summary>
+	/// Log error with `text` if `value` is true.
+	/// </summary>
+	public static void print_error_if(bool value, string text)
+	{
+		if (value)
+		{
+			Debug.LogError(text);
+		}
+	}
+}
