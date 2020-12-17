52c7bb70e243b982aeaa648bd0ad5e6fa6109be4
lessuknow <cchenonetwothree@gmail.com>
2020-12-16 21:21:16 -0500

    Player can pick up and drop items

---
+++ Assets/Gameplay/Scripts/interaction/interactalbes/interactable.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Base class for objects that can be interacted with,
+/// </summary>
+public abstract class interactable : MonoBehaviour
+{
+    public abstract void interact();
+}
