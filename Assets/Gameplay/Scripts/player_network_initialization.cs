e19ab55b9a4e93b4803d6bd763456c4b0f400c33
lessuknow <cchenonetwothree@gmail.com>
2021-01-06 21:48:33 -0500

    Pulled millmoss, now castle level exists + added box to castle level

--- Assets/Gameplay/Scripts/player_network_initialization.cs
+++ Assets/Gameplay/Scripts/player_network_initialization.cs
@@ -25,14 +25,12 @@ public class player_network_initialization : NetworkBehaviour
         {
             _player_movement.enabled = false;
             _player_camera.SetActive(false);
-            _player_interaction.enabled = false;
         }
         else
         {
             _player_movement.local_input_manager = input_manager.instance;
             _player_interaction.local_input_manager = input_manager.instance;
             _player_interaction.player_camera = _player_camera.GetComponent<Camera>();
-            Debug.Log(_player_movement.local_input_manager);
             _player_camera.GetComponent<player_camera>().local_input_manager = input_manager.instance;
         }
     }
